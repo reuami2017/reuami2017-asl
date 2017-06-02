bed7da8afa843471b6b32f267010f02ce851d844
Kesavan Kushalnagar <kesavan.kushalnagar@gmail.com>
2017-06-02 08:24:46 -0400

    Incomplete example of how to get a specific xml item in the file.

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import xml.etree.ElementTree as ET
 
-e = ET.parse('\XML_ASL_Files\(D)DINOSAUR_716.xml').getroot()
+root = ET.parse('XML_ASL_Files\(D)DINOSAUR_716.xml').getroot()
+
+print(root.tag) #check that it's on the right file, should be signs
+
+# for child in root:
+#     print(child.tag) #this should be "sign" as output
+#     for children in child:
+#         print(children.tag) #this should be frame
+#         for joint in children:
+#             print(joint.tag) #should be joint
+
+root.findall()
+for joint in root.findall('joint'):
+    print(1)
+    print(joint.get('name'))
