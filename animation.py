fc0ec73e985110ff0f71997a9b529c5a09e15e09
Shareef Ali <shareefalis@gmail.com>
2017-06-12 10:35:14 -0400

    test

--- animation.py
+++ animation.py
@@ -49,7 +49,7 @@ def make_graph(filename):
         temp.append(animation.FuncAnimation(fig, update_lines,len(ET.parse(filename).getroot()[0][0]),
                                 fargs=(data, [ax.plot(dat[0], dat[1], dat[2])[0] for dat in data]),
                                 interval=1000.0/30, blit=False))
-
+#test
 
     plt.show()
 
