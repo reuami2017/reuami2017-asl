15b165bdd01468fe7435026b11df22576dcbc214
Shareef Ali <shareefalis@gmail.com>
2017-07-06 11:38:17 -0400

    restore vasu changes

--- skelAton.py
+++ skelAton.py
@@ -21,73 +21,92 @@ def make_lines(filename):
                 d[body_part][0].append(float(joint.get("x")))
                 d[body_part][1].append(float(joint.get("y")))
                 d[body_part][2].append(float(joint.get("z")))
+
     return d
 
 def getxyzplot( body, frame):
         return [dict[body][0][frame],dict[body][1][frame],dict[body][2][frame]]
 
-def bodypart(body, body2, frame):
-    ax.plot([ getxyzplot(body,  frame)[0], getxyzplot(body2, frame)[0]],
-             [getxyzplot(body,  frame)[1], getxyzplot(body2, frame)[1]]
-            ,zs=[getxyzplot(body, frame)[2], getxyzplot(body2, frame)[2]]
-             )
+def bodypart(body, body2, frame,width_line):
+            #x
+     ax.plot([getxyzplot(body,  frame)[0], getxyzplot(body2, frame)[0]],
+            #y from                             to
+              [getxyzplot(body,  frame)[1], getxyzplot(body2, frame)[1]],
+        #z
+             zs=[getxyzplot(body, frame)[2], getxyzplot(body2, frame)[2]],linewidth=width_line)
+    #ax.setp(lines,linewidth=3.0)
+
+def bodypart_sphere(body, body2, frame, width_line):  # x
+        avg_x = (getxyzplot(body, frame)[0] + getxyzplot(body2, frame)[0])/2
+        avg_y = (getxyzplot(body, frame)[1] + getxyzplot(body2, frame)[1])/2
+        avg_z = (getxyzplot(body, frame)[2] + getxyzplot(body2, frame)[2])/2
+        ax.Cir(avg_x,avg_y,avg_z)
+
+        #ax.plot([getxyzplot(body, frame)[0], getxyzplot(body2, frame)[0]],
+        # y from                             to
+        #[getxyzplot(body, frame)[1], getxyzplot(body2, frame)[1]],
+        # z
+        #zs=[getxyzplot(body, frame)[2], getxyzplot(body2, frame)[2]], linewidth=width_line)
+
+
+# ax.setp(lines,linewidth=3.0)
+
+
 def makebody(frame):
     ax.clear()
     ## head
-    bodypart("Head", "Neck", frame)
-    bodypart("SpineShoulder", "Neck", frame)
-    bodypart("SpineShoulder", "SpineMid", frame)
-    bodypart("SpineBase", "SpineMid", frame)
+              #from    to?
+    bodypart("Head", "Neck", frame,10)
+    bodypart("SpineShoulder", "Neck", frame,10)
+    bodypart("SpineShoulder", "SpineMid", frame,25)
+    bodypart("SpineBase", "SpineMid", frame,25)
     # leg left
-    bodypart("SpineBase", "HipLeft", frame)
-    bodypart("HipLeft", "KneeLeft", frame)
-    bodypart("AnkleLeft", "KneeLeft", frame)
-    bodypart("AnkleLeft", "FootLeft", frame)
+    bodypart("SpineBase", "HipLeft", frame,5)
+    bodypart("HipLeft", "KneeLeft", frame,5)
+    bodypart("AnkleLeft", "KneeLeft", frame,5)
+    bodypart("AnkleLeft", "FootLeft", frame,5)
     #arm left
-    bodypart("SpineShoulder", "ShoulderLeft", frame)
-    bodypart("ElbowLeft", "ShoulderLeft", frame)
-    bodypart("ElbowLeft", "WristLeft", frame)
-    bodypart("HandLeft", "WristLeft", frame)
-    bodypart("HandLeft", "HandTipLeft", frame)
-    bodypart("WristLeft", "ThumbLeft", frame)
+    bodypart("SpineShoulder", "ShoulderLeft", frame,5)
+    bodypart("ElbowLeft", "ShoulderLeft", frame,5)
+    bodypart("ElbowLeft", "WristLeft", frame,5)
+    bodypart("HandLeft", "WristLeft", frame,1)
+    bodypart("HandLeft", "HandTipLeft", frame,1)
+    bodypart("WristLeft", "ThumbLeft", frame,1)
 
     ## right arm
-    bodypart("SpineShoulder", "ShoulderRight", frame)
-    bodypart("ElbowRight", "ShoulderRight", frame)
-    bodypart("ElbowRight", "WristRight", frame)
-    bodypart("HandRight", "WristRight", frame)
-    bodypart("WristRight", "ThumbRight", frame)
-    bodypart("HandRight", "HandTipRight", frame)
+    bodypart("SpineShoulder", "ShoulderRight", frame,5)
+    bodypart("ElbowRight", "ShoulderRight", frame,5)
+    bodypart("ElbowRight", "WristRight", frame,5)
+    bodypart("HandRight", "WristRight", frame,1)
+    bodypart("WristRight", "ThumbRight", frame,1)
+    bodypart("HandRight", "HandTipRight", frame,1)
 
     # leg right
-    bodypart("SpineBase", "HipRight",  frame)
-    bodypart("HipRight", "KneeRight", frame)
-    bodypart("AnkleRight", "KneeRight", frame)
-    bodypart("AnkleRight", "FootRight", frame)
+    bodypart("SpineBase", "HipRight",  frame,5)
+    bodypart("HipRight", "KneeRight", frame,5)
+    bodypart("AnkleRight", "KneeRight", frame,5)
+    bodypart("AnkleRight", "FootRight", frame,5)
+
 # dict= make_lines("XML_ASL_Files/LATER_2890.xml")
 # dict= make_lines("/DINOSAUR_716.xml")
 # dict=make_lines("edited/XML_ASL_Files/MOTHER+_1611.xml")
 # dict=make_lines("edited/XML_ASL_Files/MOTHER+FATHER_3213.xml")
 dict=make_lines("edited/XML_ASL_Files/FATHER+_1613.xml")
 
+
 fig = plt.figure()
 ax = p3.Axes3D(fig)
-ax.axis("off")  # comment this line and a later line to put back in the axis
 ax.view_init(270,270)
-
+ax.axis("off")  # comment this line and a later line to put back in the axis
 def updatefig(i):
     makebody(i)
+    ax.axis("off")  # comment this line and a later line to put back in the axis
     #ax.canvas.draw_idle()
-    # plt.pause(1000)
-    ax.axis("off")  # comment this line out to put back in the axis and a previous line (ax.axis('off')
-
-
-
+   # plt.pause(1000)
 anim = animation.FuncAnimation(fig, updatefig,frames= len(dict["Head"][0]), interval=1000/30)
 #anim.save('basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
 import threading
 
-# ax.set_axis_off()
 
 
 plt.show()
