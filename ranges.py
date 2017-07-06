67da1b3c5a149355f7bb3e36aa672c8489db5e60
reu <you@example.com>
2017-07-06 09:36:47 -0700

    added avg dist for n frames function

--- ranges.py
+++ ranges.py
@@ -18,9 +18,9 @@ def avg_distance(filename, body_part):
     avg distance
     :param filename: filename
     :param body_part: body part
-    :return: the avg distance int
+    :return: the avg distance float
     """
-    root = ET.parse("XML_ASL_Files" + "\\" + filename).getroot()
+    root = ET.parse("edited\XML_ASL_Files" + "\\" + filename).getroot()
     total = 0.0
     count = 0.0
     for sign in root:
@@ -40,8 +40,68 @@ def avg_distance(filename, body_part):
     if total == 0.0:
         print("no object found")
         return total
+
     return total/count
 
+def avg_distance_n_frames(filename, body_part, origin="SpineMid", n, first_last):
+    """
+        avg distance between 2 body parts default origin is SpineMid, which is assumed to not move, for the first/last n frames
+        :param filename: filename
+        :param body_part: body part
+        :param origin: SpineMid by default, the origin "body part"
+        :param n: number of frames
+        :param first_last: [first or last] n frames
+        :return: the avg distance float
+    """
+    root = ET.parse("edited\XML_ASL_Files" + "\\" + filename).getroot()
+    total = 0.0
+    frame_count = 0
+    n_count = 0.0
+    for sign in root:
+        #there is only one sign in each "root"
+        for frame in sign:
+            frame_count += 1
+
+        if first_last == "first":
+            for frame in sign:
+                flag = True
+                n_count += 1
+                if n_count >= n:
+                    flag = False
+                if flag:
+                    for joint in frame:
+                        if joint.get('name') == origin:
+                            spine_x = float(joint.get("x"))
+                            spine_y = float(joint.get("y"))
+                            spine_z = float(joint.get("z"))
+                        if joint.get('name') == body_part:
+                            bp_x = float(joint.get("x"))
+                            bp_y = float(joint.get("y"))
+                            bp_z = float(joint.get("z"))
+                    total += math.sqrt(((bp_x - spine_x) ** 2) + ((bp_y - spine_y) ** 2) + ((bp_z - spine_z) ** 2))
+        else:
+            for frame in sign:
+                flag = True
+                n_count += 1
+                if frame_count - n_count >= n:
+                    flag = False
+                if flag:
+                    for joint in frame:
+                        if joint.get('name') == origin:
+                            spine_x = float(joint.get("x"))
+                            spine_y = float(joint.get("y"))
+                            spine_z = float(joint.get("z"))
+                        if joint.get('name') == body_part:
+                            bp_x = float(joint.get("x"))
+                            bp_y = float(joint.get("y"))
+                            bp_z = float(joint.get("z"))
+                    total += math.sqrt(((bp_x - spine_x) ** 2) + ((bp_y - spine_y) ** 2) + ((bp_z - spine_z) ** 2))
+
+    if total == 0.0:
+        print("no object found")
+        return total
+
+    return total / count
 
 def max_arm_distance(filename):
     """
